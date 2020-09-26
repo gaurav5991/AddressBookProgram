e5826bbcfa99a3b115eb46cb10f1b4cc0e32b708
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-27 00:41:13 +0530

    [Gaurav]ADDED: Created ArrayList to Add Contact

---
+++ AddressBook.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class AddressBook 
+{
+	Scanner scan = new Scanner(System.in);
+
+	ArrayList<Contact> contact_list = new ArrayList<Contact>();
+	
+	public void setList(ArrayList<Contact> list) 
+	{
+		this.contact_list = contact_list;
+	}
+	
+	public ArrayList<Contact> getList()
+	{
+		return contact_list;
+	}
+}
