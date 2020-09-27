7244f9dbb8c6c17f65a35632d9a1d371c0405d68
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-27 09:53:47 +0530

    [Gaurav]REFACTORED: Updated Contact details

--- AddressBook.java
+++ AddressBook.java
@@ -21,4 +21,86 @@ public class AddressBook
 	{
 		contact_list.add(contact);
 	}
+	public boolean editContact(String Name)
+	{
+		int flag = 0;
+		for(Contact contact: contact_list)
+		{
+			if(contact.getFirst_name().equals(Name))
+			{
+				System.out.println("Enter the detail which needs to be updated:");
+				
+				System.out.println("1.First Name");
+				System.out.println("2.Last Name");
+				System.out.println("3.Address");
+				System.out.println("4.City");
+				System.out.println("5.State");
+				System.out.println("6.ZipCode");
+				System.out.println("7.Phone Number");
+
+				System.out.println("Choose Option");				
+
+				int choice = scan.nextInt();
+				switch(choice)
+				{
+					case 1:
+					{
+						System.out.println("Enter First Name: ");
+						String firstname = scan.next();
+						contact.setFirst_name(firstname);
+						break;
+					}
+					case 2:
+					{
+						System.out.println("Enter last name: ");
+						String lastname = scan.next();
+						contact.setLast_name(lastname);
+						break;
+					}
+					case 3:
+					{
+						System.out.println("Enter Address: ");
+						String address = scan.next();
+						contact.setAddress(address);
+						break;
+					}
+					case 4:
+					{
+						System.out.println("Enter City: ");
+						String city = scan.next();
+						contact.setCity(city);
+						break;
+					}
+					case 5:
+					{
+						System.out.println("Enter State: ");
+						String state =scan.next();
+						contact.setState(state);
+						break;
+					}
+					case 6:
+					{
+						System.out.println("Enter Zip Code: ");
+						int zip = scan.nextInt();
+						contact.setZip_code(zip);
+						break;
+					}
+					case 7:
+					{
+						System.out.println("Enter Phone Number:");
+						long phonenumber = scan.nextLong();
+						contact.setPhone_number(phonenumber);
+						break;
+					}
+				}
+				
+				flag = 1;
+				break;
+			}
+		}
+		if(flag==1)
+			return true;
+		else
+			return false;
+	}
 }
